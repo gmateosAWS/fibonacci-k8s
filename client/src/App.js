d058f877508d17185a2075f68c227667427ebb64
Gus <gmateos.aws@gmail.com>
2023-02-01 12:51:19 +0100

    k8s first commit

--- client/src/App.js
+++ client/src/App.js
@@ -10,7 +10,7 @@ function App() {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className='App-title'>Fibonacci Calculator</h1>
+          <h1 className='App-title'>Fibonacci Calculator version 2</h1>
           <Link to="/">Home</Link>&nbsp;
           <Link to="/otherpage">Other page</Link>
         </header>
